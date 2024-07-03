2570f32aa935658ff8874e13bb10e84811a29bbb
Ng Guo Xiang  (NCS) <guo.xiang.ng1@ncs.com.sg>
2024-07-03 23:05:01 +0800

    removed alias from aws provider block. Test cross account deployment

--- provider.tf
+++ provider.tf
@@ -12,7 +12,8 @@ terraform {
 
 provider "aws" {
   region = "ap-southeast-1"
-  alias = "acc1"
+  # alias = "acc1" #alias is required if TFproject requires multiple Aws providers i.e. deployment into different accounts in this context. 
+  # alias will need to be further declared in every resource block in that case.
   assume_role {
     role_arn = "arn:aws:iam::395890110782:role/lzdemo-crossacc-tfdeploy"
     external_id = "lzdemo"
